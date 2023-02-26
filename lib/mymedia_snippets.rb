587a4c0ad0cac541b6809098f23d1ec93ecda79f
James Robertson <digital.robertson@gmail.com>
2023-02-26 13:44:07 +0000

    improvement: Added a test class for more convenient testing.

--- lib/mymedia_snippets.rb
+++ lib/mymedia_snippets.rb
@@ -8,12 +8,15 @@ require 'mymedia-pages'
 
 class MyMediaSnippets < MyMediaPages
 
-  def initialize(media_type: 'snippets', config: 'mymedia.conf',
-                 css_href: '/snippets/coderay_ruby.css', lang: :ruby,
-                 log: nil, debug: false)
-
-    super(media_type: media_type, config: config, log: log, debug: debug)
-
+  #def initialize(media_type: media_type='snippets', public_type: @public_type=media_type, 
+  #            config: 'mymedia.conf', css_href: '/snippets/coderay_ruby.css', lang: :ruby, 
+  #            log: nil, debug: false)
+  def initialize(media_type: media_type='snippets', public_type: @public_type=media_type, config: 'mymedia.conf', css_href: '/snippets/coderay_ruby.css', lang: :ruby, log: nil, debug: false)
+    
+    
+    super(media_type: media_type, public_type: public_type, config: config, 
+          log: log, debug: debug)
+    #super(media_type: media_type, config: config,)
     @css_href = css_href
     @lang = lang
 
@@ -34,17 +37,41 @@ class MyMediaSnippets < MyMediaPages
 
         doc_code = Rexle.new(xml.sub('code>',"code>\n"))
         parent.insert_before doc_code.root
-        parent.delete
+        parent.delete  
       end
 
       doc
     end
 
   end
-
+  
   def add_css_js(xml)
     xml.link({rel: 'stylesheet', type: 'text/css', \
         href: @css_href, media: 'screen, projection, tv'},'')
+  end
+  
+  def writecopy_publish(s, filename=Time.now.strftime('s%d%m%yT%H%M.txt'))
+    puts 'MyMediaSnippets inside writecopy_publish' if @debug
+    super(s,filename)
+  end  
+
+end
+
+
+class SnippetsTester23 < MyMediaSnippets
+  
+  include PagesTestSetup
+  # it is assumed this class will be executed from a test directory 
+  # containing the following auxillary files:
+  #  - snippets.xsl
+  #  - index-template.html
+  
+  def initialize(config: '', cur_dir:  '', www_dir: '/tmp/www', 
+                 media_dir: '/tmp/media', debug: false)
+        
+    super(config: config, debug: debug)    
+    set_paths(cur_dir, www_dir, media_dir)    
+    
   end
 
 end
--- mymedia_snippets.gemspec
+++ mymedia_snippets.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'mymedia_snippets'
-  s.version = '0.2.0'
+  s.version = '0.3.0'
   s.summary = 'A MyMedia gem for publishing code snippets'
   s.authors = ['James Robertson']
   s.files = Dir['lib/mymedia_snippets.rb']
-  s.add_runtime_dependency('mymedia-pages', '~> 0.5', '>=0.5.4')
+  s.add_runtime_dependency('mymedia-pages', '~> 0.6', '>=0.6.1')
   s.add_runtime_dependency('coderay', '~> 1.1', '>=1.1.3')
   s.signing_key = '../privatekeys/mymedia_snippets.pem'
   s.cert_chain  = ['gem-public_cert.pem']
